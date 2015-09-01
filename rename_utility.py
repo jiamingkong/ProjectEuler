9a71deaa3a5238b47059c3c96bb734ddf45e95c2
Jiaming Kong <kongjiaming@gmail.com>
2015-09-01 08:48:44 -0400

    Renamed the files to make them sorted

---
+++ rename_utility.py
@@ -0,0 +1,13 @@
+import os
+
+def number_complete(filename):
+    _fn = filename.lower().split(".")[0].replace("problem", "")
+    serial = int(_fn)
+    return "Problem{num}.py".format(num = "%03d"%serial)
+
+for i in os.listdir(os.getcwd()):
+    if i.endswith(".py") and i.startswith("Problem"):
+        os.rename(os.path.join(os.getcwd(), i), os.path.join(os.getcwd(), number_complete(i)))
+
+        print i, ">>", number_complete(i)
+        S = raw_input()
