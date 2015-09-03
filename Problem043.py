a3b2075f8e5b6970be3873a4952bb7a0dd1f541a
Jiaming Kong <kongjiaming@gmail.com>
2015-09-03 17:47:25 -0400

    Update to Problem 44 and 62
    I have done some of these questions before and it's just not funny to
    re-code the stuff out of my memory. So I picked some unsolved ones.

--- Problem043.py
+++ Problem043.py
@@ -24,4 +24,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    # print check_number('1406357289')
---
+++ Problem062.py
@@ -0,0 +1,27 @@
+cache = {}
+
+def add_to_set(current):
+    global cache
+    number = current ** 3
+    key = ''.join(sorted(str(number)))
+    cache.setdefault(key,[])
+    cache[key].append(current)
+
+def main():
+    found = False
+    current = 100
+    while not found:
+        add_to_set(current)
+        for key,value in cache.iteritems():
+            if len(value) == 5:
+                print value[0] ** 3
+                found = True
+                break
+        current += 1
+
+
+if __name__ == '__main__':
+    # add_to_set(100)
+    # print cache
+
+    main()
