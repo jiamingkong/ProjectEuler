a4723636baca751ba082b4ce5788e93aa2bd3324
jiamingkong <kongjiaming@gmail.com>
2015-09-29 23:08:44 -0400

    Formatting
    PEP 8

--- python35/Problem048.py
+++ python35/Problem048.py
@@ -1,10 +1,13 @@
-# Oh another BigInt challenge.... I know Python can bruteforce it without a second thinking.
+# Oh another BigInt challenge.... I know Python can bruteforce it without
+# a second thinking.
+
 
 def trunc(number, digits):
     if len(str(number)) > digits:
         return number % (10**digits)
     else:
         return number
+
 
 def lazy_exp(base, power, digits):
     counter = 1
@@ -16,10 +19,11 @@ def lazy_exp(base, power, digits):
 
     return number
 
+
 def main():
     result = 0
-    for i in range(1,1001):
-        result += lazy_exp(i,i,10)
+    for i in range(1, 1001):
+        result += lazy_exp(i, i, 10)
         result = trunc(result, 10)
     print(result)
 
--- python35/Problem050.py
+++ python35/Problem050.py
@@ -1,18 +1,15 @@
 from Utilities.PrimeGenerator import PrimeGenerator, is_prime
 
-# def consecutive_sum():
-    # for i in PrimeGenerator():
-
 
 def main():
-    n,maxlen = 1000000,0
-    Set = list(PrimeGenerator(under = 10000))
+    n, maxlen = 1000000, 0
+    Set = list(PrimeGenerator(under=10000))
     newb = 2
     for a in range(4):
-        for b in range(newb,len(Set)):
-            subset  = Set[a:b]
-            s = sum(subset)  
-            if is_prime(s) and s<n and len(subset) > maxlen:
+        for b in range(newb, len(Set)):
+            subset = Set[a:b]
+            s = sum(subset)
+            if is_prime(s) and s < n and len(subset) > maxlen:
                 length = len(subset)
                 sumprime = s
                 maxlen = length
